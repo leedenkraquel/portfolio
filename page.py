b7f2734fb4176b41242edb496a9ece4bd0d30588
leedenkraquel <leedenl99@gmail.com>
2022-08-07 17:17:59 -1000

    fixed other subdirectory functions

--- page.py
+++ page.py
@@ -5,8 +5,9 @@
 #  adds the navbar and the footer to each page
 ###
 
-def write_header (page, directory, stylesheets):
-    subdirectory = "../" * directory.count("/")
+subdirectory = ""
+
+def write_header (page, stylesheets):
     page.write('''
         <head>
             <title>Leeden R | Portfolio</title>
@@ -16,7 +17,7 @@ def write_header (page, directory, stylesheets):
         page.write('<link rel="stylesheet" href="' + stylesheet + '" />')
 
     page.write('''
-            <link rel="icon" type="image/x-icon" href="images/favicon.png" />
+            <link rel="icon" type="image/x-icon" href="''' + subdirectory + '''images/favicon.png" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         </head>
     ''')
@@ -25,7 +26,7 @@ def write_header (page, directory, stylesheets):
 def write_navbar (page):
     page.write('''
         <div class="navbar">
-            <a class="menu_obj" href="#"><img class="inverted menu_button" src="images/menu.png" /></a>
+            <a class="menu_obj" href="#"><img class="inverted menu_button" src="''' + subdirectory + '''images/menu.png" alt="menu button" /></a>
             <a class="navbar_obj" href="/">Home</a>
             <a class="navbar_obj" href="/projects/">Projects</a>
             <a class="navbar_obj" href="/essays/">Essays</a>
@@ -60,8 +61,10 @@ def write_footer (page):
     return page
 
 def create_page (directory, body, stylesheets = []):
+    subdirectory = "../" * directory.count("/")
     page_html = open(directory, "w")
-    page_html = write_header(page_html, directory, stylesheets)
+    page_html.write('<!-- THIS FILE IS PROGRAMATICALLY GENERATED, CHANGES TO THIS .html FILE WILL NOT SAVE-->')
+    page_html = write_header(page_html, stylesheets)
     page_html.write('<body>')
     page_html = write_navbar(page_html)
     page_html = write_body(page_html, body)
